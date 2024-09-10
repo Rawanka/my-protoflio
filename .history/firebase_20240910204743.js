7b4f69dfb4ce77088187d735a1bb366fda7f0fbe
rawankam <rawankalharbi@gmail.com>
2024-09-10 21:27:54 +0300

    1

---
+++ .history/firebase_20240910204743.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, isSupported } from 'firebase/analytics';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwBXb6x038wnqJLwphvOJ-7hyN4uH6HFM",
+  authDomain: "portfolio-5b6f7.firebaseapp.com",
+  projectId: "portfolio-5b6f7",
+  storageBucket: "portfolio-5b6f7.appspot.com",
+  messagingSenderId: "331377484341",
+  appId: "1:331377484341:web:4cb07c6d9dfdcd8cb4877c",
+  measurementId: "G-VCNMCLK8YZ"
+};
+
---
+++ .history/public/pages/_app_20240910212324.js
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import { appWithTranslation } from 'next-i18next';
+import '../../components/i18n.js';
+import i18n from '../../components/i18n.js';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+  
+}
+// _app.js
+
+
+
+export default appWithTranslation(MyApp);
+
+
